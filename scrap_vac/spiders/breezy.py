72e9f65ddd89cb1d3a1a376b67d59a46dcbeb299
Roman Sokolov <gnonasis@gmail.com>
2026-05-18 12:28:12 +0300

    breezy.py minor improvements, add title

--- scrap_vac/spiders/breezy.py
+++ scrap_vac/spiders/breezy.py
@@ -9,21 +9,25 @@ class BreezySpider(scrapy.Spider):
     start_urls = ["https://gen-tech.breezy.hr/?&department=Development#positions"]
 
     def parse(self, response):
-        hrefs = set(response.css('a[title="Apply"]::attr(href)').getall())
-        for href in hrefs:
-            link = response.urljoin(href)
-            self.logger.info("---------------------------------------------Found link: %s", link)
-            yield scrapy.Request(
-                url=link,
-                callback=self.parse_detail_page,
-                meta={"source_url": link}
-            )
+
+        boxes = response.css('.position.transition')
+        for box in boxes:
+            href = box.css('a::attr(href)').get()
+            if href:
+                title = box.css('h2::text').get("") + " | " + " | ".join(box.css('.meta span::text').getall())
+                link = response.urljoin(href)
+                self.logger.info("---------------------------------------------Found link: %s", link)
+                yield scrapy.Request(
+                    url=link,
+                    callback=self.parse_detail_page,
+                    meta={"title": title}
+                )
 
     def parse_detail_page(self, response):
         self.logger.info("detail_page: %s", response.url)
         text = " ".join(response.css('p::text').getall())
         if re.search(r"junior", text, re.IGNORECASE) and re.search(r"python|пайтон", text, re.IGNORECASE):
             self.logger.info("match found")
-            yield {"link": response.meta["source_url"]}
+            yield {"title": response.meta["title"], "url": response.url}
         else:
             self.logger.info("No matches found")
