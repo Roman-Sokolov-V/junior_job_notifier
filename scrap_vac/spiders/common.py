c9729f446f4cc5dd5f666c5475c3bba43a3364ac
Roman Sokolov <gnonasis@gmail.com>
2026-06-28 11:08:13 +0300

    add thingsboard_ai spider

---
+++ scrap_vac/spiders/common.py
@@ -0,0 +1,28 @@
+
+class MixinCommonSpider():
+
+    @staticmethod
+    def _normalize_ws(value: str) -> str:
+        """Trim and normalize whitespace to one-line text."""
+        return " ".join(value.split()).strip()
+
+
+    @staticmethod
+    def normalize_text(text: str) -> str | None:
+        text = text.replace("\n", "").replace("\t", "").strip()
+        if text:
+            return text
+        return None
+
+
+    @classmethod
+    def clean_paragraphs(cls, paragraph_texts: list[str]) -> str:
+        return " ".join([
+            norm for p in paragraph_texts
+            if (norm := cls.normalize_text(p)) is not None
+        ])
+
+    @classmethod
+    def extract_description(cls, response, main_block_class: str) -> str:
+        paragraphs = response.css(f'.{main_block_class} ::text').getall()
+        return  cls.clean_paragraphs(paragraphs)
