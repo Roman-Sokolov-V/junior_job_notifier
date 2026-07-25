244f96d36db5d36f4788e832249f7c7b41f94507
Roman Sokolov <gnonasis@gmail.com>
2026-07-25 10:46:30 +0300

    playwright works with chromium

--- scrap_vac/settings.py
+++ scrap_vac/settings.py
@@ -104,7 +104,7 @@ DOWNLOAD_HANDLERS = {
     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
-PLAYWRIGHT_BROWSER_TYPE = "firefox"
+PLAYWRIGHT_BROWSER_TYPE = "chromium"
 PLAYWRIGHT_LAUNCH_OPTIONS = {
     "headless": True,
     #"timeout": 20 * 1000,  # 20 seconds
